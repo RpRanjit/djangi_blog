251bf5ca4bb1d819831bb05235a8f0b3bd758a8a
Ranjit Poudel <ranjitsparrow2@gmail.com>
2025-08-27 19:06:27 +0545

    change in settings

--- myblog/myblog/wsgi.py
+++ myblog/myblog/wsgi.py
@@ -8,9 +8,19 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
+# Add your project directory to the Python path
+path = '/home/your_username/djangi_blog/myblog'
+if path not in sys.path:
+    sys.path.append(path)
+
+# Set environment variables
+os.environ['DJANGO_SETTINGS_MODULE'] = 'myblog.settings'
+os.environ['SECRET_KEY'] = 'django-insecure-wd+@hagdvwy0634&)%uto$!cw#7_duib&9-$&wjp#iu2^v$4^4'
+os.environ['EMAIL_HOST_USER'] = '98160rp@gmail.com'
+os.environ['EMAIL_HOST_PASSWORD'] = 'tbqa drkm cbrh klgk'
+
+# Import and run Django
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myblog.settings')
-
 application = get_wsgi_application()
