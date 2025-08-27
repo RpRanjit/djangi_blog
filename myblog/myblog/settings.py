997c1d9a07a235e3d1c677b3a009f2e0b2eb16b0
Ranjit Poudel <ranjitsparrow2@gmail.com>
2025-08-27 14:48:26 +0545

    Second commit

--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wd+@hagdvwy0634&)%uto$!cw#7_duib&9-$&wjp#iu2^v$4^4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
